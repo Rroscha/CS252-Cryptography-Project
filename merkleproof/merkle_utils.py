6a46b16d1d52cf4b4791856d38d3fbd0bfb6d61e
Rroscha <546442393@qq.com>
2023-12-29 15:39:01 +0800

    style: adapt flake8's style

--- merkleproof/merkle_utils.py
+++ merkleproof/merkle_utils.py
@@ -1,21 +1,22 @@
 import hashlib
 
+
 class MerkleProof:
     """The Merkle Proof to be provided to a verifier to 
        prove whether the leaf is at positions pos of the 
        Merkle Tree."""
     def __init__(self, leaf, pos, hashes):
-        self.leaf = leaf     # data of leaf being checked
-        self.pos  = pos      # the position in the tree of the leaf being checked
-        self.hashes = hashes # the hashes from bottom to the top of tree for to
-                             # prove the leaf is part of the Merkle Tree
+        self.leaf = leaf      # data of leaf being checked
+        self.pos  = pos       # the position in the tree of the leaf being checked
+        self.hashes = hashes  # the hashes from bottom to the top of tree for to
+                              # prove the leaf is part of the Merkle Tree
 
 
-##  The hash prefixes in the two functions above are a security measure.
-##  They provide domain separation, meaning that the domain of a leaf hash
-##  is seperated from the domain of an internal node hash.
-##  This ensures that the verifier cannot mistake a leaf hash for 
-##  an internal node hash, and vice versa. 
+#  The hash prefixes in the two functions above are a security measure.
+#  They provide domain separation, meaning that the domain of a leaf hash
+#  is seperated from the domain of an internal node hash.
+#  This ensures that the verifier cannot mistake a leaf hash for
+#  an internal node hash, and vice versa.
 
 def hash_leaf(leaf):
     """hash a leaf value."""
@@ -32,4 +33,3 @@ def hash_internal_node(left, right):
     sha256.update(left)
     sha256.update(right)
     return sha256.digest()
-
